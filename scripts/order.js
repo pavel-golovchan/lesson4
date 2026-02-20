fe0ad467d6b63c010d1bf02f5c379fa357fa8315
pavel <pavel-golovchan@yandex.ru>
2026-02-20 22:52:50 +0300

    Отслеживание доставки 2. Создаем обработчик кнопки «Отследить»

--- scripts/order.js
+++ scripts/order.js
@@ -1,3 +1,4 @@
+//1. Поиск элементов и создание переменных
 // Переменные, нужные для работы функционала
 const trackButton = document.getElementById('trackButton');
 const trackResult = document.getElementById('trackResult');
@@ -6,3 +7,46 @@ const trackIdValue = document.getElementById('trackIdValue');
 const trackFromValue = document.getElementById('trackFromValue');
 const trackToValue = document.getElementById('trackToValue');
 const trackStatusList = document.getElementById('trackStatusList');
+
+// Основной обработчик кнопки "Отследить"
+trackButton.addEventListener('click', () => {
+if (!trackNumber.value || trackNumber.value === '') {
+    alert('Заполните номер отправления')
+}
+// 2. Создаем обработчик кнопки «Отследить»
+// Временная проверка, если результат не найден
+if (Number(trackNumber.value) < 1000 || Number(trackNumber.value) > 10000) {
+    // скрываем блок, если показывается
+    alert('К сожалению, мы не смогли найти отправление по данному номеру');
+    trackResult.classList.toggle('is-visible', false);
+    return;
+}
+
+// Тестовые данные по отправлению, в будущем получим ответ с бэкенда
+const response = {
+    id: trackNumber.value,
+    route: {
+        from: 'Москва, улица Арбат, 1',
+        to: 'Минск, проспект Независимости, 58'
+    },
+    statuses: [
+        {type: 'created', label: 'Создан', date: '10.01.2026'},
+        {type: 'in-way', label: 'В пути: Вязьма', date: '15.01.2026'},
+        {type: 'in-way', label: 'В пути: Орша', date: '16.01.2026'},
+        {type: 'in-way', label: 'В пути: Минск', date: '18.01.2026'},
+        {type: 'ready', label: 'Готов к выдаче', date: '25.01.2026'},
+        {type: 'done', label: 'Вручен', date: '27.01.2026'}
+    ]
+};
+
+// Показываем блок результата
+trackResult.classList.toggle('is-visible', true);
+
+// Заполняем основные данные
+trackIdValue.textContent = `№${response.id}`;
+trackFromValue.textContent = `Откуда: ${response.route.from}`;
+trackToValue.textContent = `Куда: ${response.route.to}`;
+
+// Рендерим ленту статусов
+// renderStatuses(response.statuses);
+});
