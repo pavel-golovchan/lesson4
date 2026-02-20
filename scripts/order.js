745ed6753f297480644599a022e8e4ce482242bf
pavel <pavel-golovchan@yandex.ru>
2026-02-20 22:51:44 +0300

    Отслеживание доставки 1. Поиск элементов и создание переменных

--- scripts/order.js
+++ scripts/order.js
@@ -1,9 +1,8 @@
-// примеры работы javascript
-alert(1)
-// найте элемент по имени класса .
-//взять первый элемент с классом main-tittle и поменяь цвет на красный
-document.getElementsByClassName('main-title')[0].style.color = 'red';
-// связать с обработчиком события клик по наденному элементу
-document.getElementsByClassName('main-title')[0].onclick = function() {
-    alert("Hello World!");
-}
+// Переменные, нужные для работы функционала
+const trackButton = document.getElementById('trackButton');
+const trackResult = document.getElementById('trackResult');
+const trackNumber = document.getElementById('trackNumber');
+const trackIdValue = document.getElementById('trackIdValue');
+const trackFromValue = document.getElementById('trackFromValue');
+const trackToValue = document.getElementById('trackToValue');
+const trackStatusList = document.getElementById('trackStatusList');
